7d5a0e59bb0636bad97361c154663df4047c7c70
SebastiansSchunke <sebi.schunke@gmail.com>
2023-03-18 17:55:38 +0100

    Add Text to Startseite(app.component.html)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
