5c824ab8d8dc02d9bdf582774cee438e527d29e4
Martin Luther <Link_Marc.student@bs-an.de>
2023-03-28 19:35:16 +0200

    links geändert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { NavComponent } from './nav/nav.component';
 import { ImprintComponent } from './Webseiten/imprint/imprint.component';
+import { TermineComponent } from './termine/termine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     NavComponent,
-    ImprintComponent
+    ImprintComponent,
+    TermineComponent
   ],
   imports: [
     BrowserModule
