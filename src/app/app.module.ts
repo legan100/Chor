6961adedca754ae7726c083a3c6bc5448a460ea2
SebastiansSchunke <sebi.schunke@gmail.com>
2023-03-28 14:24:28 +0200

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
