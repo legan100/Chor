6b734abfb8ef77597bb207227a51a53f316a30ae
SEBBI555 <sebastian.schunke@hotmail.com>
2023-04-01 15:33:31 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { NavComponent } from './nav/nav.component';
+import { AboutUsComponent } from './Webseiten/about-us/about-us.component';
 import { ImprintComponent } from './Webseiten/imprint/imprint.component';
-import { TermineComponent } from './termine/termine.component';
+import { TermineComponent } from './Webseiten/termine/termine.component';
 import { StartseiteComponent } from './startseite/startseite.component';
-import { Router, RouterModule, Routes } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
-/*const appRoute:Routes = [
-{path: 'Termine', component: TermineComponent},
-{path: 'Ueber', component: AboutUsComponent},
-{path: 'Impressum', component:ImprintComponent},
-]*/
+import { MenuAppComponent } from './menu-app/menu-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
-    NavComponent,
     ImprintComponent,
     TermineComponent,
-    StartseiteComponent
+    StartseiteComponent,
+    MenuAppComponent
   ],
   imports: [
-    BrowserModule,
-    //RouterModule.forRoot(appRoute)
+    BrowserModule
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
