759e26785bea28dd0f5b7b2839459ba8000dc83b
SEBBI555 <sebastian.schunke@hotmail.com>
2023-04-21 18:49:44 +0200

    add Dropdown Menü

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AboutUsComponent } from './Webseiten/about-us/about-us.component';
 import { ImprintComponent } from './Webseiten/imprint/imprint.component';
 import { TermineComponent } from './Webseiten/termine/termine.component';
 import { StartseiteComponent } from './startseite/startseite.component';
-import { MenuAppComponent } from './menu-app/menu-app.component';
+import { MenuAppComponent } from './menu-body/menu-app.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 const appRoute:Routes = [
   {path: 'termine', component: TermineComponent},
   {path: '', component: AboutUsComponent},
   {path: 'impressum', component:ImprintComponent},
+  {path: 'menu-body', component:MenuAppComponent},
   {path: '**', redirectTo: '/termine' }
   ]
 
@@ -27,7 +31,10 @@ const appRoute:Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoute)
+    RouterModule.forRoot(appRoute),
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
