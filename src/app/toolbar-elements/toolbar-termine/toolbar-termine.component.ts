bc095934273599bbb5ffe27bed680d5936247327
SEBBI555 <sebastian.schunke@hotmail.com>
2023-04-22 16:09:35 +0200

    change files

---
+++ src/app/toolbar-elements/toolbar-termine/toolbar-termine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-termine',
+  templateUrl: './toolbar-termine.component.html',
+  styleUrls: ['./toolbar-termine.component.css']
+})
+export class ToolbarTermineComponent {
+
+}
