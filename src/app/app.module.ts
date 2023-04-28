1ffda9999a9449f1369470c2f17f7b8475a8f523
SEBBI555 <sebastian.schunke@hotmail.com>
2023-04-28 22:23:18 +0200

    finished website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,16 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-const appRoute:Routes = [
-  {path: 'impressum', component:ImprintComponent},
-  {path: 'about-us', component:AboutUsComponent},
-  {path: 'termine', component:TermineComponent},
-  {path: '', component:StartseiteComponent},
-  //{path: 'toolbar-about-us', component:MenuAppComponent}
- // {path: '**', redirectTo: '/termine' }
-  ]
+const appRoute: Routes = [
+  { path: 'impressum', component: ImprintComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'termine', component: TermineComponent },
+  { path: 'startseite', component: StartseiteComponent },
+  { path: '**', redirectTo: '/startseite' }
+]
 
 @NgModule({
   declarations: [
@@ -37,10 +36,10 @@ const appRoute:Routes = [
     MatButtonModule,
     MatSlideToggleModule
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 
 
-export class AppModule { }  
+export class AppModule { }
 
